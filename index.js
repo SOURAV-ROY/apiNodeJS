b080daf5db86ae01adeb14fd768bed1957570a44
SOURAV ROY <souravroy.info13@gmail.com>
2020-12-22 17:51:53 +0600

    Json get By Content Type

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const HTTP = require('http');
 
+const todos = [
+    {id: 1, value: "One"},
+    {id: 2, value: "Two"},
+    {id: 3, value: "Three"},
+    {id: 4, value: "Four"},
+]
+
 const SERVER = HTTP.createServer((req, res) => {
     // console.log(req.method);
-    const {headers, url, method} = req;
-    console.log(headers, url, method);
-    res.end();
+    // const {headers, url, method} = req;
+    // console.log(headers, url, method);
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Powered-By', 'Node.js');
+    res.write("<h1>Hello Sourav</h1>")
+    res.write("<h2>Hello Sourav Again</h2>")
+    res.end(JSON.stringify({
+        success: true,
+        data: todos
+    }));
 })
 
 const PORT = 2020;
