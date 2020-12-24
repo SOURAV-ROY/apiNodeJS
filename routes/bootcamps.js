6bd0eb68168098437cb70274da9fb4515cb5f7e6
SOURAV ROY <souravroy.info13@gmail.com>
2020-12-24 15:14:05 +0600

    Using The Express Router WOW ✔🚀

---
+++ routes/bootcamps.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+        // res.send("<h1>Hello Sourav Roy</h1>");
+        // res.send({name: "SOURAV"});
+        // res.json({name: "SOURAV JSON"});
+        // res.sendStatus(401);
+        // res.status(400).json({success: false});
+        // res.status(401).json({success: false});
+        // res.status(200).json({success: true, data: {id: 1, name: "SOURAV"}});
+        res.status(200).json({success: true, msg: "Show All Bootcamps"});
+    }
+);
+router.get('/:id', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            msg: `Show a Single Bootcamp ${req.params.id}`
+        });
+});
+
+router.post('/', (req, res) => {
+    res.status(200)
+        .json({
+            success: true,
+            msg: "Create New Bootcamp"
+        });
+});
+
+router.put('/:id', (req, res) => {
+    res.status(200)
+        .json({success: true, msg: `Update Bootcamp ${req.params.id}`});
+});
+
+router.delete('/:id', (req, res) => {
+    res.status(200)
+        .json({success: true, msg: `Delete Bootcamp ${req.params.id}`});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,48 +1,16 @@
 const express = require('express');
 const dotenv = require("dotenv");
 
+//Router Files****************
+const bootcamps = require('./routes/bootcamps');
+
 //Load env vars ************
 dotenv.config({path: "./config/config.env"});
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-        // res.send("<h1>Hello Sourav Roy</h1>");
-        // res.send({name: "SOURAV"});
-        // res.json({name: "SOURAV JSON"});
-        // res.sendStatus(401);
-        // res.status(400).json({success: false});
-        // res.status(401).json({success: false});
-        // res.status(200).json({success: true, data: {id: 1, name: "SOURAV"}});
-        res.status(200).json({success: true, msg: "Show All Bootcamps"});
-    }
-);
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200)
-        .json({
-            success: true,
-            msg: `Show a Single Bootcamp ${req.params.id}`
-        });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200)
-        .json({
-            success: true,
-            msg: "Create New Bootcamp"
-        });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200)
-        .json({success: true, msg: `Update Bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200)
-        .json({success: true, msg: `Delete Bootcamp ${req.params.id}`});
-});
-
+//Mount Routers ******************
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
