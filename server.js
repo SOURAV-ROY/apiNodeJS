768c6efdffd4bc3e01fb3e1fb3aee2f76d0258fc
SOURAV ROY <souravroy.info13@gmail.com>
2020-12-25 17:31:26 +0600

    Use morgan middleware with custom middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require("dotenv");
 const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 //Router Files****************
 const bootcamps = require('./routes/bootcamps');
@@ -12,6 +13,10 @@ const app = express();
 
 //Use Middleware ******************
 app.use(logger);
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 //Mount Routers ******************
 app.use('/api/v1/bootcamps', bootcamps);
