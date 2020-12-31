06da381c31705e96309620aa1728ca306b289989
SOURAV ROY <souravroy.info13@gmail.com>
2021-01-01 02:24:23 +0600

    GeoJSON Location Geocoder Hook - MapQuest API✔💚

--- models/BootcampModel.js
+++ models/BootcampModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -37,25 +38,23 @@ const BootcampSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please Add Address"],
     },
-    // location: {
-    //     //GeoJSON Point *************************
-    //     type: {
-    //         type: String, // Don't do `{ location: { type: String } }`
-    //         enum: ['Point'], // 'location.type' must be 'Point'
-    //         required: true
-    //     },
-    //     coordinates: {
-    //         type: [Number],
-    //         required: true,
-    //         index: '2dsphere'
-    //     },
-    //     formattedAddress: String,
-    //     street: String,
-    //     city: String,
-    //     state: String,
-    //     zipcode: String,
-    //     country: String,
-    // },
+    location: {
+        //GeoJSON Point *************************
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String,
+    },
     careers: {
         //Array of string **************
         type: [String],
@@ -103,6 +102,7 @@ const BootcampSchema = new mongoose.Schema({
 
 });
 
+// Use Slugify ********************************************************************
 BootcampSchema.pre('save', function (next) {
 
     this.slug = slugify(this.name, {lower: true});
@@ -111,5 +111,25 @@ BootcampSchema.pre('save', function (next) {
 
     next();
 });
+
+//GeoCode & create location field *******************************
+BootcampSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode
+    }
+
+    //Do not save address in DB ***************
+    this.address = undefined;
+
+    next();
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
