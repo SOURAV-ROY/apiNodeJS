3cc4aad33b1729945c0b496c8504c4d3775e3687
SOURAV ROY <souravroy.info13@gmail.com>
2021-01-18 15:19:53 +0600

    Logout To Clear Token Cookie WoW

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,6 +63,21 @@ exports.login = asyncHandler(async (req, res, next) => {
     // })
 });
 
+// @description     Logout User / Clear Cookie
+// @route           GET /api/v1/auth/logout
+// @access          Private
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
+
 // @description     Get User
 // @route           GET /api/v1/auth/me
 // @access          Private
--- middleware/auth.js
+++ middleware/auth.js
@@ -5,19 +5,22 @@ const User = require('../models/UserModel');
 
 //Protect Routes ************************************
 exports.protect = asyncHandler(async (req, res, next) => {
+
     let token;
+
     if (req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
+        //Set Token from Bearer token In Header ****************
         token = req.headers.authorization.split(' ')[1];
+        //Set Token From Cookie ********************************
+    } else if (req.cookies.token) {
+        token = req.cookies.token;
     }
-    // else if (req.cookies.token) {
-    //     token = req.cookies.token;
-    // }
 
     // Make sure token Exits ***********************************************
     if (!token) {
-        return next(new ErrorResponse('Not Authorize to access this route', 401));
+        return next(new ErrorResponse('Not Authorized to access this route', 401));
     }
     try {
         // Verify Token ****************************************
@@ -30,7 +33,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
         next();
 
     } catch (errors) {
-        return next(new ErrorResponse('Not Authorize to access this route', 401));
+        return next(new ErrorResponse('Not Authorized to access this route', 401));
     }
 });
 
@@ -38,7 +41,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 exports.authorize = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
-            return next(new ErrorResponse(`User Role ${req.user.role} is Not Authorize to access this route`, 403));
+            return next(new ErrorResponse(`User Role ${req.user.role} is Not Authorized to access this route`, 403));
         }
         next();
     };
