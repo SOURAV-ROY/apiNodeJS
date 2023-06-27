f30b99e95ca7daf6f2968ad028dc908daef11da7
SOURAV-ROY <souravroy.info13@gmail.com>
2023-06-27 14:18:32 +0600

    Plugin Version Update

--- _data/seed.js
+++ _data/seed.js
@@ -1,44 +1,45 @@
-const { faker } = require("@faker-js/faker");
+const {faker} = require("@faker-js/faker");
 // const CountryIndexingModel = require('../models/CountryIndexingModel');
-const ClaimTypeModel = require("../models/ClaimTypeModel");
+const UserModel = require("../models/UserModel");
 const mongoose = require("mongoose");
 require("colors");
 
 async function seedData() {
-  // Connection URL
-  const uri = process.env.MONGO_URI;
-  const seed_count = 5000;
-  mongoose
-    .connect(uri, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected To MongoDB".cyan.bold);
-    })
-    .catch((err) => {
-      console.log("ERROR".red.bold, err);
+    // Connection URL
+    const uri = "String";
+    const seedCount = 5000;
+    mongoose
+        .connect(uri, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("Connected To MongoDB".cyan.bold);
+        })
+        .catch((err) => {
+            console.log("ERROR".red.bold, err);
+        });
+
+    let timeSeriesData = [];
+    // create 5000 fake data
+    for (let i = 0; i < seedCount; i++) {
+        const name = faker.person.firstName();
+        const email = faker.internet.email();
+        timeSeriesData.push({name, email});
+    }
+
+    console.log(timeSeriesData);
+    const seedDB = async () => {
+        await UserModel.insertMany(timeSeriesData);
+    };
+
+    seedDB().then(() => {
+        mongoose.connection.close();
     });
-
-  let timeSeriesData = [];
-  // create 5000 fake data
-  for (let i = 0; i < seed_count; i++) {
-    const name = faker.name.firstName();
-    const email = faker.internet.email();
-    timeSeriesData.push({name, email});
-  }
-
-  const seedDB = async () => {
-    await CountryIndexingModel.insertMany(timeSeriesData);
-  };
-
-  seedDB().then(() => {
-    mongoose.connection.close();
-  });
 }
 
 seedData().then(() => {
-  console.log("SUCCESSFULLY INSERT".green.bold);
+    console.log("SUCCESSFULLY INSERT".green.bold);
 });
--- db/db.js
+++ db/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     const connect = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
         useUnifiedTopology: true
     });
 
