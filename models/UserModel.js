d1a49dc4ff4190b5f664c3682c06f7dee4d15661
SOURAV ROY <souravroy.info13@gmail.com>
2023-08-03 15:40:52 +0000

    User model update

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,79 +1,80 @@
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please Add A Name Please"]
+      type: String,
+      required: [true, "Please Add A Name Please"],
     },
     email: {
-        type: String,
-        required: [true, "Please Add A An email"],
-        unique: true,
-        match: [
-            /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-            "Please add a valid email"
-        ]
+      type: String,
+      required: [true, "Please Add A An email"],
+      unique: true,
+      match: [/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Please add a valid email"],
     },
     role: {
-        type: String,
-        enum: ['user', 'publisher'],
-        default: 'user',
+      type: String,
+      enum: ["user", "publisher"],
+      default: "user",
     },
     password: {
-        type: String,
-        required: [true, "Please Add A Password"],
-        minlength: 6,
-        select: false
+      type: String,
+      required: [true, "Please Add A Password"],
+      minlength: 6,
+      select: false,
     },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
     createdAt: {
-        type: Date,
-        default: Date.now()
-    }
-});
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
 
 // Encrypt password using bcryptjs **********************
-UserSchema.pre('save', async function (next) {
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    if (!this.isModified('password')) {
-        next();
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 //Sign JWT and Return *********************************
 UserSchema.methods.getSignedJwtToken = function () {
-    return jwt.sign({id: this._id, name: this.name}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE
-    });
+  return jwt.sign({ id: this._id, name: this.name }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
 };
 
 //Match user entered password to hashed password ***************
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 //Generate and Hash Password Token ******************************************
 UserSchema.methods.getResetPasswordToken = function () {
-// Generate Token **********************************************************
-    const resetToken = crypto.randomBytes(20).toString('hex');
+  // Generate Token **********************************************************
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-// Has token and resetPasswordToken field **********************************
-    this.resetPasswordToken = crypto
-        .createHash('sha256')
-        .update(resetToken)
-        .digest('hex');
+  // Has token and resetPasswordToken field **********************************
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-// Set Expire Time ********************************************************
-    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  // Set Expire Time ********************************************************
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
-    return resetToken;
+  return resetToken;
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
