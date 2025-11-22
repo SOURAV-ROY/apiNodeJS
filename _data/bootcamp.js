7360a50636171150b6bd60fe9f8a48866effa741
Sourav Roy <sourav.roy@technonext.com>
2025-11-22 18:19:39 +0600

    feat: Add geocoding utility, data seeding script, and example environment configuration.

--- _data/bootcamp.js
+++ _data/bootcamp.js
@@ -3,9 +3,9 @@ const Bootcamp = require("../models/BootcampModel");
 const mongoose = require("mongoose");
 // import { v4 } from "uuid";
 
-// const uri = "mongodb://127.0.0.1:27017/bnodeapi";
-const uri = "mongodb://127.0.0.1:27017/bnodeapi_test";
-const seedCount = 10000;
+const uri = "mongodb://127.0.0.1:27017/bnodeapi";
+// const uri = "mongodb://127.0.0.1:27017/bnodeapi_test";
+const seedCount = 1000;
 const batchSize = 100;
 
 // Generate 5 fake bootcamps
