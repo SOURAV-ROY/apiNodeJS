ae46cb5f405994ec44e160373272a47ca4c8073b
Sourav Roy <sourav.roy@technonext.com>
2025-11-22 18:28:50 +0600

    feat: Add Mongoose Bootcamp model with schema definition, slugification, geocoding, and cascade delete functionality.

--- models/BootcampModel.js
+++ models/BootcampModel.js
@@ -115,24 +115,24 @@ BootcampSchema.pre("save", function (next) {
 });
 
 //GeoCode & create location field *******************************
-// BootcampSchema.pre("save", async function (next) {
-//   const loc = await geocoder.geocode(this.address);
-//   this.location = {
-//     type: "Point",
-//     coordinates: [loc[0].longitude, loc[0].latitude],
-//     formattedAddress: loc[0].formattedAddress,
-//     street: loc[0].streetName,
-//     city: loc[0].city,
-//     state: loc[0].stateCode,
-//     zipcode: loc[0].zipcode,
-//     country: loc[0].countryCode,
-//   };
-//
-//   //Do not save address in DB ***************
-//   this.address = undefined;
-//
-//   next();
-// });
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  //Do not save address in DB ***************
+  this.address = undefined;
+
+  next();
+});
 
 //Cascade delete courses when a bootcamp *************
 BootcampSchema.pre("remove", async function (next) {
